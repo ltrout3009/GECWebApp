6dff52c3e8c011cd5af4e9883c37c112346a9c5a
Layne Trout <ltrout@grueneenviro.com>
2023-11-07 09:09:37 -0600

    Test

---
+++ resources/views/testpage.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<title>Laravel Testing Website</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <h1>Hello World!</h1>
+
+    <a class="button" href="/admin/dashboard">Dashboard</a>
+</body>
